aedeb187551f8ac052bf9aa49d6f791910410324
Kosteyyy <7456052@gmail.com>
2024-10-29 13:57:32 +0300

    обработка файлов и clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const loader = require("sass-loader");
 
 module.exports = {
   entry: "./src/index.js",
@@ -9,9 +10,22 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "imgs/[name].[hash].[ext]",
+        }
+      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
